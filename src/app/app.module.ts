4bdaf25dadf40290612781e858a98e5456215c85
Brian Fernandes <fbrian0707@gamil.com>
2021-07-06 16:27:38 +0100

    Added firebase settings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from "./core/core.module";
+import { AdminModule } from "./admin/admin.module";
+import { SharedModule } from "./shared/shared.module";
+import { ShoppingModule } from "./shopping/shopping.module";
+import { AngularFireModule } from "@angular/fire";
+import { environment } from "src/environments/environment";
 
 @NgModule({
   declarations: [
@@ -10,7 +16,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule,
+    AdminModule,
+    SharedModule,
+    ShoppingModule,
+    AngularFireModule.initializeApp(environment.config)
   ],
   providers: [],
   bootstrap: [AppComponent]
